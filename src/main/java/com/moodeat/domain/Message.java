69518e97c6a4f8fc426f45023aed80364f320a80
gyuwonMoon <s201801402@hufs-gsuite.kr>
2024-12-14 03:06:29 +0900

    chore: Message 클래스에 Builder, Data 어노테이션 추가

--- src/main/java/com/moodeat/domain/Message.java
+++ src/main/java/com/moodeat/domain/Message.java
@@ -2,6 +2,11 @@ package com.moodeat.domain;
 
 import com.moodeat.domain.enums.MessageRole;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Message {
 	private MessageRole role;
 	private String content;
