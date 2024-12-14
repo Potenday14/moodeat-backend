65ab974c4ba82e0826646164e6eda7d6031aff39
gyuwonMoon <s201801402@hufs-gsuite.kr>
2024-12-14 19:37:35 +0900

    feat. 프롬프트 개선(반말모드, 응답개수 증가) 및 에러 시 디폴트 응답 반환

--- src/main/java/com/moodeat/service/ClovaService.java
+++ src/main/java/com/moodeat/service/ClovaService.java
@@ -43,21 +43,25 @@ public class ClovaService {
 
 		Map<String, String> systemMessageMap = new HashMap<>();
 		systemMessageMap.put("role", "system");
-		systemMessageMap.put("content",
-			"당신은 사용자의 기분과 상황에 맞는 메뉴를 추천하기 위한 어시스턴트입니다."
-				+ " 아래 순서를 바탕으로 레시피 추천 정보를 담은 json 객체를 반환해주세요. \n"
-				+ "1. 사용자의 감정과 사용자가 선택한 재료, 대화 내용을 바탕으로, 레시피 리스트에서 레시피를 추천해주세요."
-				+ " (레시피 리스트의 개수가 5개 이하인 경우 반드시 모든 레시피 추천, "
-				+ " 레시피 리스트 개수가 5개 이상인 경우 반드시 최소 5개 이상, 최대 7개 이하 레시피 추천"
-				+ "2. 사용자가 선택한 재료, 상황에 맞는 키워드를 2개 생성해주세요. \n"
-				+ "3. 마지막으로 키워드를 포함한 추천 문구를 친절한 마케터처럼 작성해주세요. (한글 기준 30자 이내)"
-				+ " 키워드에 포함되는 부분은 <b> 태그로 강조해주세요.\n"
-				+ "반환 형식은 반드시 지정된 형식의 json 객체여야 합니다.\n\n"
-				+ "[입력값 예시]\n사용자의 감정: 기쁨\n재료 리스트: [새우, 두부]\n"
-				+ "레시피 리스트: {1=새우 두부 계란찜, 2=부추 콩가루 찜, 3=방울토마토 두부 샐러드}\n\n"
-				+ "[출력값 예시(동일 형식의 json 객체만 반환)]\n{\"recipe_ids\": [1, 3], \"keywords\": [기쁨, 합격], "
-				+ "\"reason\": \"<b>기쁜 합격 소식</b>이 있는 날 맛있는  <b>두부</b>로 만든 술 안주 어때요?\"}\n\n");
 
+		StringBuilder systemContent = new StringBuilder();
+		systemContent.append("당신은 사용자의 기분과 상황에 맞는 메뉴를 추천하기 위한 어시스턴트입니다.");
+		systemContent.append(" 아래 순서에 따라 레시피 추천 정보를 담은 json 객체를 반환해주세요.\n\n");
+		systemContent.append("1. 레시피 맵의 사이즈를 파악하세요.\n");
+		systemContent.append("2. 사용자의 감정과 사용자가 선택한 재료, 대화 내용을 바탕으로, 레시피 맵에서 레시피를 추천해주세요.\n");
+		systemContent.append("2-1. 레시피 맵의 사이즈가 5 이하인 경우, 모든 레시피를 추천하세요.\n");
+		systemContent.append("2-2. 레시피 맵의 사이즈가 5 이상인 경우, 최소 5개 이상, 최대 8개 이하의 레시피를 추천하세요.\n");
+		systemContent.append("3. 마지막으로 추천 레시피 목록에 대해 키워드를 포함한 추천 문구를 친절한 마케터처럼 작성해주세요.");
+		systemContent.append(" 문구는 한글 기준 30자 이내, 반말로 작성해야 합니다.");
+		systemContent.append(" 키워드에 포함되는 부분은 <b> 태그로 강조해주세요.\n");
+		systemContent.append("4. 최종 반환 형식은 반드시 {\"recipe_ids\": number[], \"keywords\": string[], \"reason\": string}");
+		systemContent.append(" 형태의 json 객체만을 하나의 string 타입으로 반환해야 합니다.\n\n");
+		systemContent.append("[입력값 예시]\n사용자의 감정: 기쁨\n재료 리스트: [새우, 두부]\n");
+		systemContent.append("레시피 맵: {1=새우 두부 계란찜, 2=부추 콩가루 찜, 3=방울토마토 두부 샐러드}\n\n");
+		systemContent.append("[출력값 예시]\n{\"recipe_ids\": [1, 2, 3], \"keywords\": [\"기쁨\", \"합격\"],");
+		systemContent.append(" \"reason\": \"<b>기쁜 합격 소식</b>이 있는 날 맛있는 <b>콩</b>으로 만든 건강한 술 안주 어때?\"}");
+
+		systemMessageMap.put("content", systemContent.toString());
 		requestMessages.add(systemMessageMap);
 
 		for (MessageDto message : messages) {
@@ -69,7 +73,7 @@ public class ClovaService {
 
 		Map<String, String> tmpMap = new HashMap<>();
 		tmpMap.put("role", "user");
-		tmpMap.put("content", String.format("사용자의 감정: %s\n재료 리스트: %s\n레시피 리스트: %s", mood, ingredients, menu));
+		tmpMap.put("content", String.format("사용자의 감정: %s\n재료 리스트: %s\n레시피 맵: %s", mood, ingredients, menu));
 		requestMessages.add(tmpMap);
 
 		// request 생성
@@ -102,36 +106,57 @@ public class ClovaService {
 				ObjectMapper objectMapper = new ObjectMapper();
 				JsonNode root = objectMapper.readTree(response.getBody());
 
-				System.out.println(root);
-				// content만 파싱
-				JsonNode result = objectMapper.readTree(root.path("result")
+				// 결과의 content만 파싱
+				JsonNode content = objectMapper.readTree(root.path("result")
 					.path("message")
 					.findValuesAsText("content")
 					.get(0));
 
-				// JSON에서 "recipe_ids", "keywords", "reason" 값을 추출하여 Map에 저장
-				JsonNode recipeIdsNode = result.path("recipe_ids");
-				List<Integer> recipeIds = new ArrayList<>();
-				recipeIdsNode.forEach(node -> recipeIds.add(node.asInt()));
-
-				JsonNode keywordsNode = result.path("keywords");
-				List<String> keywords = new ArrayList<>();
-				keywordsNode.forEach(node -> keywords.add(node.asText()));
-
-				String reason = result.path("reason").asText();
-
-				// 결과를 Map에 저장
-				Map<String, Object> resultMap = new HashMap<>();
-				resultMap.put("recipe_ids", recipeIds);
-				resultMap.put("keywords", keywords);
-				resultMap.put("reason", reason);
-
-				return resultMap;
+				return parseJsonToResultMap(content);
 			} else {
 				throw new RuntimeException("Clova API 호출 실패: " + response.getStatusCode());
 			}
 		} catch (Exception e) {
-			throw new RuntimeException("Clova API 호출 중 오류 발생", e);
+			// default 응답 반환
+			Map<String, Object> resultMap = new HashMap<>();
+			resultMap.put("recipe_ids", List.of(5, 6, 7, 8, 94));
+			resultMap.put("keywords", List.of("기본값"));
+			resultMap.put("reason", "오늘 같은 날 든든하고 속 풀리는 따듯한 <b>국물 요리</b> 어때?");
+
+			return resultMap;
 		}
+	}
+
+	Map<String, Object> parseJsonToResultMap(JsonNode node) throws Exception {
+		// JSON에서 "recipe_ids", "keywords", "reason" 값을 추출하여 Map에 저장
+		JsonNode recipeIdsNode = node.path("recipe_ids");
+		List<Integer> recipeIds = new ArrayList<>();
+		recipeIdsNode.forEach(r -> recipeIds.add(r.asInt()));
+
+		if (recipeIds.isEmpty()) {
+			throw new Exception("recipe not exists");
+		}
+
+		JsonNode keywordsNode = node.path("keywords");
+		List<String> keywords = new ArrayList<>();
+		keywordsNode.forEach(k -> keywords.add(k.asText()));
+
+		if (keywords.isEmpty()) {
+			throw new Exception("keywords not exists");
+		}
+
+		String reason = node.path("reason").asText();
+
+		if (reason.isEmpty()) {
+			throw new Exception("reason not exists");
+		}
+
+		// 결과를 Map에 저장
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("recipe_ids", recipeIds);
+		resultMap.put("keywords", keywords);
+		resultMap.put("reason", reason);
+
+		return resultMap;
 	}
 }
